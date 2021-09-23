6b3b3ea8c6b2f358383d9e09661b53a256a2b9ed
Ilham Muhammad <ilhamhd98@gmail.com>
2021-09-23 20:23:03 +0700

    adding controller and updating routes

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Book;
+
+class BookController extends Controller
+{
+    public function create(Request $request)
+    {
+        $this->validate($request, [
+            "name" => "required|unique:book",
+            "price" => "required",
+            "status" => "required"
+        ]);
+
+        $data = $request->all();
+        $book = Book::create($data);
+
+        return response()->json($book);
+    }
+
+    public function index()
+    {
+        $book = Book::all();
+        return response()->json($book);
+    }
+
+    public function show($id)
+    {
+        $book = Book::find($id);
+        return response()->json($book);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $book = Book::find($id);
+
+        if (!$book) {
+            return response()->json(['message' => 'Data not found'], 404);
+        }
+
+        $this->validate($request, [
+            "name" => "required|unique:book",
+            "price" => "required",
+            "status" => "required"
+        ]);
+
+        $data = $request->all();
+        $book->fill($data);
+        $book->save();
+
+        return response()->json($book);
+    }
+
+    public function destroy($id)
+    {
+        $book = Book::find($id);
+
+        if (!$book) {
+            return response()->json(['message' => 'Data not found'], 404);
+        }
+
+        $book->delete();
+
+        return response()->json(['message' => 'Data deleted successfully'], 200);
+    }
+}
